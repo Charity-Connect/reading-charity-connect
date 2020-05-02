500ac5188f39165fef28c5eaf2d75908c859c0a3
Martin Millmore <martin.millmore@oracle.com>
2020-05-02 20:28:36 +0100

    removed login for now

--- rest/session_check.php
+++ rest/session_check.php
@@ -1,5 +1,12 @@
 <?php
 
+global $dev_headers;
+
+if($dev_headers){
+	if(!isset($_SERVER["REMOTE_ADDR"])){
+	 	header("Access-Control-Allow-Origin: localhost:8000");
+	}
+}
 	session_start();
 	if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
 		echo "true";
--- rest/session_login.php
+++ rest/session_login.php
@@ -2,6 +2,13 @@
 
 include_once $_SERVER['DOCUMENT_ROOT'] .'/config/dbclass.php';
 include_once $_SERVER['DOCUMENT_ROOT'] .'/lib/common.php';
+
+global $dev_headers;
+
+$dev_headers=true;
+echo "true";
+exit;
+
 $dbclass = new DBClass();
 $connection = $dbclass->getConnection();
 
