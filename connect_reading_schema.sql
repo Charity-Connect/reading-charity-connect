1ea9a14138561a1cddcfb430f7461fb4d6eb20d7
Martin Millmore <martin.millmore@oracle.com>
2020-06-13 10:38:07 +0100

    removed string

--- connect_reading_schema.sql
+++ connect_reading_schema.sql
@@ -9,9 +9,6 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `iaoo4tce_connect_reading`
---
 
 -- --------------------------------------------------------
 
