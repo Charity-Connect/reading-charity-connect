e9bc62e04bef6914b10008a5b2d7f6250d613f83
Martin Millmore <martin.millmore@oracle.com>
2020-06-14 11:20:04 +0100

    added __DIR__

--- lib/cron.php
+++ lib/cron.php
@@ -1,6 +1,6 @@
 <?php
 require_once 'common.php';
-require_once '../config/dbclass.php';
+require_once __DIR__.'../config/dbclass.php';
 require_once 'Cron/CronExpression.php';
 date_default_timezone_set("Europe/London");
 
